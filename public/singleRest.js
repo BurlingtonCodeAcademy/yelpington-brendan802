27e9bdcd9bf0ae981f5fe579a85c0026681c08ee
brendan802 <67173637+brendan802@users.noreply.github.com>
2020-10-25 18:27:12 -0400

    restaurant info update

--- public/singleRest.js
+++ public/singleRest.js
@@ -1,4 +1,3 @@
-
 //fetching restaurant names to links in nav bar
 let navbar = document.getElementById("nav-links");
 fetch("./api/restaurants")
@@ -8,14 +7,13 @@ fetch("./api/restaurants")
   .then((allNames) => {
     allNames.forEach((restaurant) => {
       let resid = restaurant.id;
-      let resname= restaurant.name;
+      let resname = restaurant.name;
       console.log(resname);
       let anchor = document.createElement("a");
       let listName = document.createElement("li");
       anchor.href = "/" + resid;
-      anchor.onclick = 
-      listName.textContent = restaurant.name;
-      navbar.appendChild(anchor)
+      anchor.onclick = listName.textContent = restaurant.name;
+      navbar.appendChild(anchor);
       anchor.appendChild(listName);
     });
   });
@@ -29,41 +27,51 @@ L.tileLayer("https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png", {
     '&copy; Openstreetmap France | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(yelpMap);
 
-
 //restaurant id fetch for pin placement
-let id = document.location.pathname.slice(1)
-console.log(id)
+let id = document.location.pathname.slice(1);
+console.log(id);
 
 fetch("/api/" + id)
-.then((res) => {
+  .then((res) => {
     return res.json();
-})//as callback function here
-.then(pinGenerator)
+  }) //as callback function here
+  .then(pinGenerator);
 
 //pin generator function
-function pinGenerator(restaurant){
-console.log(restaurant)
-let longitude = restaurant.longitude
-let latitude = restaurant.latitude
-//converting those variables to integers
-long = parseFloat(longitude) 
-lat = parseFloat(latitude) 
+//restaurant info generator
+function pinGenerator(restaurant) {
+  console.log(restaurant);
+  let longitude = restaurant.longitude;
+  let latitude = restaurant.latitude;
+  //converting those variables to integers
+  long = parseFloat(longitude);
+  lat = parseFloat(latitude);
+  let restaurantName = restaurant.name;
+  console.log(restaurant);
+  //restaurant info generated to page
+  let resttitle = document.getElementById("restTitle");
+  let restnotes = document.getElementById("restNotes");
+  let restaddress = document.getElementById("restAddress");
+  let resthours = document.getElementById("restHours");
+  let restphone = document.getElementById("restPhone");
 
-let restaurantName = restaurant.name
-console.log(restaurant)
-console.log(restaurantName)
-let restaurantMarker = L.marker([lat, long]).addTo(yelpMap);
-restaurantMarker.bindPopup(`<h2>${restaurantName}</h2>`);
+  let title = restaurant.name;
+  let notes = restaurant.notes;
+  let hours = restaurant.hours;
+  let address = restaurant.address;
+  let phone = restaurant.phonenumber;
+  //inner html
+  resttitle.innerHTML = title;
+  restaddress.innerHTML = address;
+  resthours.innerHTML = hours;
+  restnotes.innerHTML = notes;
+  restphone.innerHTML = phone;
+
+  let restaurantMarker = L.marker([lat, long]).addTo(yelpMap);
+  restaurantMarker.bindPopup(`<h2>${restaurantName}</h2>`);
 }
+
 
 
 //need to make pin links
 //need to pull restaurant info
-
-
-
-
-
-
-
-   
